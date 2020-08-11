89831a1bb114d7ee3f6759d8ddd5e6e101d64489
teamblue-e2 <teamblue@online.de>
2020-08-11 23:18:13 +0200

    beautify print functions

--- lib/python/Screens/Screen.py
+++ lib/python/Screens/Screen.py
@@ -116,7 +116,7 @@ class Screen(dict):
 			self.session.close(self, *retval)
 
 	def show(self):
-		print(("[Screen] Showing screen '%s'." % self.skinName))  # To ease identification of screens.
+		print("[Screen] Showing screen '%s'." % self.skinName)  # To ease identification of screens.
 		# DEBUG: if (self.shown and self.alreadyShown) or not self.instance:
 		if (self.shown and self.already_shown) or not self.instance:
 			return
@@ -247,7 +247,7 @@ class Screen(dict):
 				if not updateonly:
 					val.GUIcreate(parent)
 				if not val.applySkin(desktop, self):
-					print(("[Screen] Warning: Skin is missing renderer '%s' in %s." % (val, str(self))))
+					print("[Screen] Warning: Skin is missing renderer '%s' in %s." % (val, str(self)))
 		for key in self:
 			val = self[key]
 			if isinstance(val, GUIComponent):
@@ -256,10 +256,10 @@ class Screen(dict):
 				depr = val.deprecationInfo
 				if val.applySkin(desktop, self):
 					if depr:
-						print(("[Screen] WARNING: OBSOLETE COMPONENT '%s' USED IN SKIN. USE '%s' INSTEAD!" % (key, depr[0])))
-						print(("[Screen] OBSOLETE COMPONENT WILL BE REMOVED %s, PLEASE UPDATE!" % depr[1]))
+						print("[Screen] WARNING: OBSOLETE COMPONENT '%s' USED IN SKIN. USE '%s' INSTEAD!" % (key, depr[0]))
+						print("[Screen] OBSOLETE COMPONENT WILL BE REMOVED %s, PLEASE UPDATE!" % depr[1])
 				elif not depr:
-					print(("[Screen] Warning: Skin is missing element '%s' in %s." % (key, str(self))))
+					print("[Screen] Warning: Skin is missing element '%s' in %s." % (key, str(self)))
 		for w in self.additionalWidgets:
 			if not updateonly:
 				w.instance = w.widget(parent)
